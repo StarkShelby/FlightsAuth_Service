b1152a86560560ad4723f70c21974cf4ba2c023a
StarkShelby <your-email@example.com>
2026-08-08 17:00:36 +0530

    added disauthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const { InfoController } = require("../../controllers");
 const userRoute = require("./userRoute");
+const { AuthMiddleware } = require("../../middlewares");
 
 const router = express.Router();
 
-router.use("/info", InfoController.info);
+router.use("/info", AuthMiddleware.checkAuth, InfoController.info);
 router.use("/user", userRoute);
 
 module.exports = router;
--- src/utils/common/Auth.js
+++ src/utils/common/Auth.js
@@ -20,7 +20,17 @@ async function createJWTToken(input) {
   }
 }
 
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, ServerConfig.JWT_SECRET);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 module.exports = {
   checkPassword,
   createJWTToken,
+  verifyToken,
 };
