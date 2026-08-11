033f8f086f148fb5f51fa12902833f7fe7e2304a
StarkShelby <your-email@example.com>
2026-08-11 16:20:23 +0530

    Created Roles for the User, using the through table.

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Roles, { through: "User_Role", as: "role" });
     }
   }
   User.init(
--- src/utils/common/enums.js
+++ src/utils/common/enums.js
@@ -1,1 +1,7 @@
-module.exports = {};
+User_Roles = {
+  ADMIN: "admin",
+  CUSTOMER: "customer",
+  FLIGHT_COMPANY: "flight_company",
+};
+
+module.exports = { User_Roles };
